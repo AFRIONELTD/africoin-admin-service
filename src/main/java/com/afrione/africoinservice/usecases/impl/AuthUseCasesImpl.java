99c17322eba4067d3e9cb06eadf732939cd74124
Felix Adewale <adewalefelixtaiwo@gmail.com>
2025-12-16 12:24:47 +0100

    prevent token assignment for users requiring password change

--- src/main/java/com/afrione/africoinservice/usecases/impl/AuthUseCasesImpl.java
+++ src/main/java/com/afrione/africoinservice/usecases/impl/AuthUseCasesImpl.java
@@ -58,10 +58,6 @@ public class AuthUseCasesImpl implements AuthUseCases {
                 throw new BadRequestException("Account is locked due to too many failed login attempts. Please contact support.");
             }
 
-//            if (user.isRequiresPasswordChange()) {
-//                throw new BadRequestException("Password change required. Please reset your password.");
-//            }
-
             if (user.getRecordStatus() != RecordStatusConstant.ACTIVE) {
                 log.warn("Login attempt for inactive user: {}", request.getEmail());
                 throw new BadRequestException("User account is not active");
@@ -126,7 +122,9 @@ public class AuthUseCasesImpl implements AuthUseCases {
                     .build();
 
             LoginResponse response = new LoginResponse();
-            response.setUserToken(customerToken);
+            if (!user.isRequiresPasswordChange()) {
+                response.setUserToken(customerToken);
+            }
             response.setUser(portalUserModel);
 
             log.info("User logged in successfully: {}", request.getEmail());
