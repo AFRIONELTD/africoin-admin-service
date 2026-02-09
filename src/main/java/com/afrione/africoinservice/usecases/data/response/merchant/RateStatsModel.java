9abb66dca2ed808e53e39ed37973545bf19207fa
Felix Adewale <adewalefelixtaiwo@gmail.com>
2026-02-09 14:49:40 +0100

    refactor: change lastUpdated field type to String in RateStatsModel

--- src/main/java/com/afrione/africoinservice/usecases/data/response/merchant/RateStatsModel.java
+++ src/main/java/com/afrione/africoinservice/usecases/data/response/merchant/RateStatsModel.java
@@ -1,19 +1,16 @@
 package com.afrione.africoinservice.usecases.data.response.merchant;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 public class RateStatsModel {
     private long activeCurrencies;
     private BigDecimal avgMarkup;
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime lastUpdated;
+    private String lastUpdated;
 
 }
 
