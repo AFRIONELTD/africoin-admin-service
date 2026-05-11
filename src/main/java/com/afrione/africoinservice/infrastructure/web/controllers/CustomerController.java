29f6e6584b180907f52dcda3a9287159a900934c
Bisi <adebisi@paydestal.com>
2026-05-11 08:51:43 +0100

    Update existing endpoint

--- src/main/java/com/afrione/africoinservice/infrastructure/web/controllers/CustomerController.java
+++ src/main/java/com/afrione/africoinservice/infrastructure/web/controllers/CustomerController.java
@@ -75,7 +75,7 @@ public class CustomerController {
         @NotNull
         Boolean isApproved;
         @NotBlank
-        @Pattern(regexp = "SELFIE|ID")
+        @Pattern(regexp = "SELFIE|ID|POA|GENERAL", message = "Invalid file type")
         String fileType;
 
         public UserDocReviewRequest toRequest() {
