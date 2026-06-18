0f1e1fe5ebf4291223dd51f2ac089907ceb3d3e8
Bisi <adebisi@paydestal.com>
2026-06-18 19:19:37 +0100

    Update existing endpoint

--- src/main/java/com/afrione/africoinservice/domain/services/ApplicationProperty.java
+++ src/main/java/com/afrione/africoinservice/domain/services/ApplicationProperty.java
@@ -47,6 +47,12 @@ public class ApplicationProperty {
     }
 
     public String geCustomerTokenSecretKey() {
+        return  "ec39fe4cd9b69453d5a856890d0f8e8cda95706db413ac108e87d3a7e567ad44";
+//        return environmentService.getVariable("TOKEN_B2C_SECRET_KEY", "ec39fe4cd9b69453d5a856890d0f8e8cda95706db413ac108e87d3a7e567ad44");
+    }
+
+
+    public String getTestCustomerTokenSecretKey() {
 
         return environmentService.getVariable("TOKEN_B2C_SECRET_KEY", "ec39fe4cd9b69453d5a856890d0f8e8cda95706db413ac108e87d3a7e567ad44");
     }
